f85cd7991fb99a7a9f199bd85b931b460f4dab15
Chuck Thomas <chuckthemole@gmail.com>
2023-10-10 12:54:37 -0700

    Adding empty prod db

--- websiteChuck/settings.py
+++ websiteChuck/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = 'websiteChuck.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+DATABASE = {}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql_psycopg2',
